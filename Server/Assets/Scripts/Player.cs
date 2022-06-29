fc10f1dbc8d3f0e9e79aa0a3de74af685485daf1
csd3410 <csd3410@csd.uoc.gr>
2022-06-29 11:40:46 +0300

    Add MessageExtension, modify Player

--- Server/Assets/Scripts/Player.cs
+++ Server/Assets/Scripts/Player.cs
@@ -16,20 +16,35 @@ public class Player : MonoBehaviour
 
     public static void Spawn(ushort id, string username)
     {
+        foreach (Player otherPlayer in list.Values)
+            otherPlayer.SendSpawned(id);
+
         Player player = Instantiate(GameLogic.Singleton.PlayerPrefab, new Vector3(0.0f, 1.0f, 0.0f), Quaternion.identity).GetComponent<Player>();
         player.name = $"Player {id} ({(string.IsNullOrEmpty(username) ? "Guest" : username)})";
         player.Id = id;
         player.Username = string.IsNullOrEmpty(username) ? $"Guest {id}" : username;
 
+        player.SendSpawned();
         list.Add(id, player);
     }
 
     #region Messages
     private void SendSpawned()
     {
-        Message msg = Message.Create(MessageSendMode.reliable, (ushort)ServerToClientId.playerSpawned);
+       NetworkManager.Singleton.Server.SendToAll(AddSpawnData(Message.Create(MessageSendMode.reliable, (ushort)ServerToClientId.playerSpawned)));
+    }
+
+    private void SendSpawned(ushort toClientId)
+    {
+        NetworkManager.Singleton.Server.Send(AddSpawnData(Message.Create(MessageSendMode.reliable, (ushort)ServerToClientId.playerSpawned)), toClientId);
+    }
+
+    private Message AddSpawnData(Message msg)
+    {
         msg.AddUShort(Id);
         msg.AddString(Username);
+        msg.AddVector3(transform.position);
+        return msg;
     }
 
     [MessageHandler((ushort)ClientToServerId.name)]
